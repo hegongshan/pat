fea70ad0f86141cecb2aae0e096f7160c46654ac
hegongshan <hegongshan@qq.com>
2020-08-08 17:49:49 +0800

    Add: A1116.Come on! Let's C (20)

---
+++ advanced/A1116.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <map>
+#include <set>
+using namespace std;
+
+bool isPrime(int x) {
+	if (x < 2) {
+		return false;
+	}
+	for (int i = 2; i <= sqrt(x); i++) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	map<string, int> ranklist;
+	for (int i = 1; i <= n; i++) {
+		string id;
+		cin >> id;
+
+		ranklist[id] = i;
+	}
+
+	int k;
+	cin >> k;
+	set<string> queryIds;
+	for (int i = 0; i < k; i++) {
+		string queryId;
+		cin >> queryId;
+
+		cout << queryId << ": ";
+		// 如果已经查过了
+		if (queryIds.count(queryId)) {
+			cout << "Checked";
+		} else if (ranklist.count(queryId)) {
+			int rank = ranklist[queryId];
+			queryIds.insert(queryId);
+
+			if (rank == 1) {
+				cout << "Mystery Award";
+			} else if (isPrime(rank)) {
+				cout << "Minion";
+			} else {
+				cout << "Chocolate";
+			}
+		} else { // 如果query ID不存在
+			cout << "Are you kidding?";
+		}
+
+		cout << endl;
+	}
+	return 0;
+}
